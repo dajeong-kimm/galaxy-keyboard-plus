b29153f326f4dc87bb6f629d27d05dd9f1a40ce0
최영환 <dlsxj101@naver.com>
2025-04-28 23:40:51 +0900

    docs: backup 폴더 삭제

--- poc/mcp-web-app-backup/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  sendCommand: (data) => ipcRenderer.invoke('run-command', data),
-  selectProjectFolder: () => ipcRenderer.invoke('select-folder'),
-});
